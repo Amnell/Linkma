e4527ca65fef70fdd0cfc7b2c2f8379532a4b6bf
Mathias Amnell <Mathias.Amnell@ikano.se>
2023-08-07 11:27:48 +0200

    Initial Commit

---
+++ Linkma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Linkma
+//
+//  Created by Mathias Amnell on 2023-08-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Linkma/LinkmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkmaApp.swift
+//  Linkma
+//
+//  Created by Mathias Amnell on 2023-08-07.
+//
+
+import SwiftUI
+
+@main
+struct LinkmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
